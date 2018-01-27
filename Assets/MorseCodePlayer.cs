0621c801596f8ffe039cdb35345eef7664009faf
Kep Amun <Kep@EdifyGames.com>
2018-01-27 13:34:54 -0500

    Initial character controller, Morse code player, usable item trigger system.

---
+++ Assets/MorseCodePlayer.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorseCodePlayer : MonoBehaviour
+{
+    public AudioClip DotSound;
+    public AudioClip DashSound;
+
+    public SpriteRenderer TargetSpriteRenderer;
+
+    float _dotDuration = 0.09f;
+    float _dashDuration;
+    float _spaceDuration;
+
+    AudioSource _audio;
+
+
+    Dictionary<char, string> _characterCodes = new Dictionary<char, string> {
+    {'A',"._"},
+    {'B',"_..."},
+    {'C',"_._."},
+    {'D',"_.."},
+    {'E',"."},
+    {'F',".._."},
+    {'G',"__."},
+    {'H',"...."},
+    {'I',".."},
+    {'J',".___"},
+    {'K',"_._"},
+    {'L',"._.."},
+    {'M',"__"},
+    {'N',"_."},
+    {'O',"___"},
+    {'P',".__."},
+    {'Q',"__._"},
+    {'R',"._."},
+    {'S',"..."},
+    {'T',"_"},
+    {'U',".._"},
+    {'V',"..._"},
+    {'W',".__"},
+    {'X',"_.._"},
+    {'Y',"_.__"},
+    {'Z',"__.."},
+    {'1',"._"},
+    {'2',".._"},
+    {'3',"..._"},
+    {'4',"...._"},
+    {'5',"....."},
+    {'6',"_...."},
+    {'7',"_..."},
+    {'8',"_.."},
+    {'9',"_."},
+    {'0',"_____"},
+    };
+
+
+    private void Awake()
+    {
+        _dashDuration = _dotDuration * 3;
+        _spaceDuration = _dotDuration * 7;
+
+        _audio = GetComponent<AudioSource>();
+
+        StartPlayingMessage("SOS SOS");
+    }
+
+
+    public void StartPlayingMessage(string message)
+    {
+        StartCoroutine(PlayMessage(message));
+    }
+
+
+    IEnumerator PlayMessage(string message)
+    {
+        foreach(var character in message)
+        {
+            if(char.IsWhiteSpace(character))
+            {
+                yield return new WaitForSeconds(_spaceDuration);
+            }
+            else if(_characterCodes.ContainsKey(character))
+            {
+                string code = _characterCodes[character];
+
+                foreach(var c in code)
+                {
+                    TargetSpriteRenderer.enabled = true;
+
+                    if(c == '.')
+                    {
+                        _audio.PlayOneShot(DotSound);
+                        yield return new WaitForSeconds(_dotDuration);
+                    }
+                    else
+                    {
+                        _audio.PlayOneShot(DashSound);
+                        yield return new WaitForSeconds(_dashDuration);
+                    }
+
+                    TargetSpriteRenderer.enabled = false;
+
+                    yield return new WaitForSeconds(_dotDuration);
+                }
+            }
+        }
+
+    }
+    
+
+}
---
+++ Assets/TopDownCharacterController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public Text TargetUseText;
+
+    Rigidbody2D _rigidbody;
+
+    
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+
+	void Update ()
+    {
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 v = new Vector2();
+        v.x = Input.GetAxis("Horizontal") * 100;
+        v.y = Input.GetAxis("Vertical") * 100;
+
+        _rigidbody.AddForce(v);
+
+        if(v.sqrMagnitude > 0.001f)
+        {
+            _rigidbody.drag = 20;
+        }
+        else
+        {
+            _rigidbody.drag = 1000;
+        }
+        
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Usable usable = collision.GetComponent<Usable>();
+
+        if(usable != null)
+        {
+            TargetUseText.enabled = true;
+            TargetUseText.text = usable.DisplayName;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        TargetUseText.enabled = false;
+    }
+
+}
